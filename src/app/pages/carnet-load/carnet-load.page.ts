45946a26adf54d5fd498fa1bc25de7f73fe99bc4
KevinCastro <chicho9808@gmail.com>
2022-03-14 12:31:19 -0300

    .

---
+++ src/app/pages/carnet-load/carnet-load.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carnet-load',
+  templateUrl: './carnet-load.page.html',
+  styleUrls: ['./carnet-load.page.scss'],
+})
+export class CarnetLoadPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
