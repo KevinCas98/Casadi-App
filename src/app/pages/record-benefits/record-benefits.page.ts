ede4515bbdf36f3f8448ca39ac282ad299e22be7
KevinCastro <chicho9808@gmail.com>
2022-03-15 14:08:33 -0300

    .

---
+++ src/app/pages/record-benefits/record-benefits.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-record-benefits',
+  templateUrl: './record-benefits.page.html',
+  styleUrls: ['./record-benefits.page.scss'],
+})
+export class RecordBenefitsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
