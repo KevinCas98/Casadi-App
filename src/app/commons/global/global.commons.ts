908b6b9281653dae2368678242423c03e75dbcd3
KevinCastro <chicho9808@gmail.com>
2022-03-17 14:04:16 -0300

    .

--- src/app/commons/global/global.commons.ts
+++ src/app/commons/global/global.commons.ts
@@ -8,7 +8,7 @@ export class GlobalCommon {
   private baseAPiUrl: string;
 
   constructor() { 
-    this.baseAPiUrl = "http://localhost:80/casadi/api/";
+    this.baseAPiUrl = "http://localhost/casadi/api/";
   }
 
   public getBaseApiUrl(){
