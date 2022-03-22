429d006d5f79fa6868c5c3b7ba951ca2d3916ff5
Mauro <mauro.garcia@riskiq.net>
2022-03-22 12:34:49 -0300

    add register user

---
+++ src/app/services/api/user/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { GlobalCommon } from '../../../commons/global/global.commons';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UserService {
+
+  httpHeader = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(public global : GlobalCommon, private http: HttpClient) { }
+
+  public registerUser(formData: FormData):Observable<any>{
+    return this.http.post<{ data: any[] }>(this.global.getBaseApiUrl()+'users/register', formData)
+  }
+
+}
